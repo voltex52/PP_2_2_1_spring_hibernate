d2eae5e8b102959f8591d667dc76830894de4562
voltex52 <voltex7@yandex.ru>
2022-08-24 15:41:08 +0300

    LastRework

--- 2.2.1. spring_hibernate/src/main/java/hiber/service/UserServiceImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/service/UserServiceImp.java
@@ -12,8 +12,10 @@ import java.util.List;
 @Service
 public class UserServiceImp implements UserService {
 
-   @Autowired
    private UserDao userDao;
+   public UserServiceImp (UserDao userDao) {
+      this.userDao = userDao;
+   }
 
    @Transactional
    @Override
